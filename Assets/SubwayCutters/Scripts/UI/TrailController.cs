18e999ad0d94b492e600050a14d39ef9f6ab0f0e
KensDevlog <kenarigo@yahoo.com>
2026-06-27 21:01:38 -0700

    ADDED trail effect and controller

---
+++ Assets/SubwayCutters/Scripts/UI/TrailController.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+public class TrailController : MonoBehaviour
+{
+    private bool _inputDown = false;
+    Vector2 _inputPos = Vector2.zero;
+    
+    private void Update()
+    {
+#if UNITY_EDITOR
+        _inputPos = Input.mousePosition;
+        _inputDown = Input.GetMouseButton(0);
+#else
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            inputPos = Input.GetTouch(0).position;
+            inputBegan = true;
+        }
+#endif
+        
+        if (_inputDown)
+        {
+            Vector3 worldPos = TouchToWorldSpace(_inputPos);
+            transform.position = worldPos;
+        }
+    }
+    
+    Vector3 TouchToWorldSpace(Vector2 screenPos)
+    {
+        Vector3 screenWithDepth = new Vector3(screenPos.x, screenPos.y, Camera.main.WorldToScreenPoint(transform.position).z);
+        return Camera.main.ScreenToWorldPoint(screenWithDepth);
+    }
+}
